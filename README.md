7e58f919a93836382d108915047296935d3222aa
Dipayan <dipayan90@yahoo.com>
2019-12-29 19:01:49 -0800

    Refactoring stuff

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # ml-model-server
+
+This project provides you a pre-built **REST** server to score your machine learning models. 
+
+## Features
+
+* Score models by just providing configurations. 
+* Error handling and standard logging inbuilt. 
+* Comes bootstrapped with **CORS** support for cross domain access.
+* Documentation is inbuilt in the form of **SWAGGER** docs.
+* Supports following python model serialization formats:
+    - PMML
+    - Pickle ( Assumes that a `predict` function exists )
+    - Simple lookup.
+    
+## Installation
+
+```bash
+pip install ml-model-server[all]
+``` 
+
+The above command installs all dependencies. However if you want a smaller footprint, you can select packages you want to install:
+
+|  Functionality | Command  |
+|---------|----------------------------------------|
+|  PMML   | `pip install ml-model-server[pmml]`    |
+|  Pickle | `pip install ml-model-server[pickle]`  |
+|  All    | `pip install ml-model-server[all]`     |
---
+++ setup.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""Configuration file for the package."""
+from setuptools import setup, find_packages
+
+core = [
+    'flask-restplus==0.13.0',
+    'Flask-Cors==3.0.8'
+]
+
+aws = [
+    'botocore==1.12.253',
+    'boto3==1.9.253',
+]
+
+pmml = (core + ['pypmml==0.9.3'])
+pickle = (core + ['dill==0.3.1.1'])
+s3 = aws
+
+all_deps = (core + pmml + s3 + pickle)
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(name='ml-model-server',
+      version='1.0',
+      url='https://github.com/dipayan90/ml-model-server',
+      author='Dipayan Chattopadhyay',
+      author_email='dipayan90@gmail.com',
+      description='Cookie cutter online ML model serving solution',
+      long_description=LONG_DESCRIPTION,
+      packages=find_packages(),
+      package_data={
+          '': ['setup.py', 'setup.cfg'],
+          'ml-model-server': ['*.py']
+      },
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      setup_requires=["pytest-runner==4.4"],
+      install_requires=core,
+      extras_require={
+          'all': all_deps,
+          's3': s3,
+          'pmml': pmml,
+          'pickle': pickle
+      },
+      tests_require=[
+        'pytest==4.4.1',
+        'pytest-cov==2.6.1',
+        'pytest-mock==1.10.4',
+        'requests-mock==1.6.0',
+        'pytest-bdd==3.1.1',
+        'moto==1.3.8',
+      ],
+      include_package_data=True,
+      zip_safe=False)
